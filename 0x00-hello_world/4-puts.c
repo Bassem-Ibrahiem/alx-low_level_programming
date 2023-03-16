96dda31b1f5a95412ffd90af28947894cc667f88
Bama <bamainlove>
2023-03-17 00:57:57 +0200

    Beginning in C

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+/*writing the Hello message to in main with put() function*/
+int main()
+{
+    puts("\"Programming is like building a multilingual puzzle");
+    return 0;
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+/*writing the Hello message to in main with prinf() function*/
+main()
+{
+    printf("with proper grammar, but the outcome is a piece of art,\n");
+    return 0;
+}
