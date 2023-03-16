1b8631e6f0b58750f765b6a1fa8c7087ca372cfb
Bama <bamainlove>
2023-03-17 01:20:56 +0200

    Beginning in C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,2 @@
-#include <stdio.h>
-/*writing the Hello message to in main with put() function*/
-int main(void)
-{
-    puts("\"Programming is like building a multilingual puzzle");
-    return (0);
+{#include <stdio.h>
+int main(void) {puts("\"Programming is like building a multilingual puzzle"); return 0;}}
