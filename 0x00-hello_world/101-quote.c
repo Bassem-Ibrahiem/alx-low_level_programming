897112bee7cad2236feebbeeb6c57d8516e8d322
Bama <bamainlove>
2023-03-17 03:49:42 +0200

    Beginning in C

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,14 +2,14 @@
 /**
  * main - Entry point
  *
- * Description: prints and that piece of art is useful" 
- * - Dora Korpar, 2015-10-19
+ * Description: prints "and that piece of art is useful\" 
+ * - Dora Korpar, 2015-10-19"
  * 
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-    write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", \
-          strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
-    return (1);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", \
+			strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
+	return (1);
 }
