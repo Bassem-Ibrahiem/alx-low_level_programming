60551d7d3bc8c063482bfee9165595538382076b
Bama <bamainlove>
2023-03-18 01:57:28 +0200

    C - Variables, if, else, while

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Prints lowercase alphabet except ("e" and "q").
+ *
+ * Return: Always 0.
+*/
+
+int main(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+		if (a == 'e' || a == 'q')
+			a;
+		else
+			putchar(a);
+	putchar('\n');
+	return (0);
+}
