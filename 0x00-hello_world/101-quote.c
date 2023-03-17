cd11c394a3cbcf01088366700473a08fed7bf2d5
Bama <bamainlove>
2023-03-17 03:54:59 +0200

    Beginning in C

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
+#include <unistd.h>
+
 /**
  * main - Entry point
  *
- * Description: prints "and that piece of art is useful\" 
- * - Dora Korpar, 2015-10-19"
+ * Description: prints Dora Korpar quote
  * 
  * Return: Always 0 (Success)
  */
