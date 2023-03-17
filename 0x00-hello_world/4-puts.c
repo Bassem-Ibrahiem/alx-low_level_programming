2ad0c96680127ce57b0697a8b965f210377a5691
Bama <bamainlove>
2023-03-17 02:11:16 +0200

    Beginning in C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- *main in beginning
- *return 0 always
+ *main: in beginning
+ *return: 0 always
  */
 int main(void)
 {puts("\"Programming is like building a multilingual puzzle");
