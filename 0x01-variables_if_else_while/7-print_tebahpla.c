e0d1a17478d7403b0ab0d9beb69c29564d9addf9
Bama <bamainlove>
2023-03-18 03:14:03 +0200

    C - Variables, if, else, while

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Prints lowercase alphabet.
+ *
+ * Return: Always 0.
+*/
+
+int main(void)
+{
+	char a;
+
+	for (a = 'z'; a >= 'a'; a--)
+		putchar(a);
+	putchar('\n');
+	return (0);
+}
