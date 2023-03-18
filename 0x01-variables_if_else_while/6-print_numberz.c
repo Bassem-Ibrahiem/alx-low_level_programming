f6ddc052620a4cfa9ee2f8d9a88d729141ed95b9
Bama <bamainlove>
2023-03-18 03:01:06 +0200

    C - Variables, if, else, while

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -10,12 +10,12 @@
 
 int main(void)
 {
-	int i;
+	int num;
 
-	for (i = 0; i < 10; i++)
-		putchar(i + '0');
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
 
-	putchar("\n");
-	
+	putchar('\n');
+
 	return (0);
 }
