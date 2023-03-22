08ce05ea0628b48e1f0a5c540a5f6993d335fb8a
Bama <bamainlove>
2023-03-22 02:21:03 +0200

    Functions and Nested loops

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,13 +8,15 @@
 
 void print_alphabet_x10(void)
 {
-	int i, char a;
+	int i;
+	char a;
 
 	for (i = 0; i < 10; i++)
 	{
+			
 		for (a = 'a'; a <= 'z'; a++)
 			_putchar(a);
 	}
-
+	
 	putchar('\n');
 }
