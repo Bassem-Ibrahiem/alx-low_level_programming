44960ba8f1537389192441316a1a906cc44acb45
Bama <bamainlove>
2023-03-22 18:56:46 +0200

    C - Debugging

---
+++ 0x03-debugging/0-main.h
@@ -0,0 +1,15 @@
+#ifndef Main_H
+#define Main_H
+
+/**
+ * File: main.h
+ *
+ * Including: declarations for
+ * all used functions.
+*/
+
+#include <stdio.h>
+
+void positive_or_negative(int i);
+
+#endif
