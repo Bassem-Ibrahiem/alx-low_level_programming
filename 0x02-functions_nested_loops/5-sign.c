6ed2cdb2a138bf6148743cac3a18a4e88692f21a
Bama <bamainlove>
2023-03-22 04:08:58 +0200

    Functions and Nested loops

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,7 +2,7 @@
 
 /**
  * print_sign - prints the sign of a number
- * n@: whether it is?
+ * @n: whether it is?
  *
  * Return: as it is?
 */
--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * _abs - computes the absolute value of an integer.
+ * _abs - computes the absolute value of
+ *@a: as an integer
  *
  * Return: as it is?
 */
