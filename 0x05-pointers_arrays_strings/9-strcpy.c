dc414310afed8b136ca6ac237446b60856aaac57
Bama <bamainlove>
2023-03-27 14:55:29 +0200

    C - Pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isupper - check that if
+ * @c: is upper case
+ *
+ * Return: as it is?
+*/
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+		return (1);
+
+	return (0);
+}
