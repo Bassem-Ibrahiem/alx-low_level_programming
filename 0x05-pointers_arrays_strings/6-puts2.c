fbe2453191ae654e0ac5b411586e10827fc74540
Bama <bamainlove>
2023-03-28 00:50:59 +0200

    C - Pointers, arrays and strings

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -3,7 +3,7 @@
 /**
  * puts2 - function that prints
  * every other character of
- * @strr: as a string, starting with the first character
+ * @str: as a string, starting with the first character
  * , followed by a new line.
  *
  * Return: as it is?
@@ -13,7 +13,7 @@ void puts2(char *str)
 {
 	int i;
 
-	for (i = 0; str[i] != '\0';i+=2)
+	for (i = 0; str[i] != '\0'; i += 2)
 		_putchar(str[i]);
 
 	_putchar('\n');
