c827e0028cf3a11a940ae35284277631aaf2a9c5
Bama <bamainlove>
2023-03-28 02:35:13 +0200

    C - Pointers, arrays and strings

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -22,9 +22,9 @@ int main(void)
 		pass[i] = rand() % 82;
 		s += (pass[i] + '0');
 		putchar(pass[i] + '0');
-		if ((3831 - s) - '0' < 82)
+		if ((3832 - s) - '0' < 82)
 		{
-			n = 3831 - s - '0';
+			n = 3832 - s - '0';
 			s += n;
 			putchar(n + '0');
 			break;
