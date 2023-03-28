691892511cc3a5325127f426e207e0f1242c5ec3
Bama <bamainlove>
2023-03-28 02:24:30 +0200

    C - Pointers, arrays and strings

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,16 +1,44 @@
 #include "main.h"
 
 /**
- * _isupper - check that if
- * @c: is upper case
+ * _atoi - function that convert
+ * @s: as a string to an integer.
  *
  * Return: as it is?
 */
 
-int _isupper(int c)
+int _atoi(char *s)
 {
-	if (c >= 65 && c <= 90)
-		return (1);
+	int n, l, c, d, x, g;
 
-	return (0);
+	n = 0;
+	l = 0;
+	c = 0;
+	d = 1;
+	x = 0;
+	g = 1;
+
+	while (s[l] != '\0')
+	{
+		if (s[l] >= '0' && s[l] <= '9')
+		{
+			c++;
+			if (!(s[l + 1] >= '0' && s[l + 1] <= '9'))
+				break;
+		}
+		l++;
+	}
+	for (; c > 1; c--)
+		d *= 10;
+	for (; x <= l; x++)
+	{
+		if (s[x] == '-')
+			g *= -1;
+		else if (s[x] <= '9' && s[x] >= '0')
+		{
+			n += (s[x] - '0') * d * g;
+			d /= 10;
+		}
+	}
+	return (n);
 }
