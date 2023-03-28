52d6c37e8927d1dcde69b307ebf0b6af3aee4e23
Bama <bamainlove>
2023-03-28 02:33:07 +0200

    C - Pointers, arrays and strings

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,16 +1,35 @@
-#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 /**
- * _isupper - check that if
- * @c: is upper case
+ * main - program that generates random valid
+ * passwords for the program 101-crackme
  *
- * Return: as it is?
-*/
-
-int _isupper(int c)
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
-	if (c >= 65 && c <= 90)
-		return (1);
+	int pass[100];
+	int i, s, n;
+
+	s = 0;	
+
+	srand(time(NULL));
+
+	for (i = 0; i < 100; i++)
+	{
+		pass[i] = rand() % 82;
+		s += (pass[i] + '0');
+		putchar(pass[i] + '0');
+		if ((3831 - s) - '0' < 82)
+		{
+			n = 3831 - s - '0';
+			s += n;
+			putchar(n + '0');
+			break;
+		}
+	}
 
 	return (0);
 }
