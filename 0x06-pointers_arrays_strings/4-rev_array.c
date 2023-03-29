73b381c9398a16530bc8d57d0b64894031b59f03
Bama <bamainlove>
2023-03-29 23:13:30 +0200

    C - More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -12,10 +12,10 @@ void reverse_array(int *a, int n)
 {
 	int i, x;
 
-	for (i = n - n; i < n / 2; i++)
+	for (i = n - 1; i >= n / 2; i--)
 	{
 		x = a[i];
-		a[i] = a[n - 1];
+		a[i] = a[n - i - 1];
 		a[n - i - 1] = x;
 	}
 }
