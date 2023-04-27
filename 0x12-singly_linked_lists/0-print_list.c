83ae6f64efa0808297c96a11b2fffa60af52ac2e
Bassem <Bassem.ibrahiem@gmail.com>
2023-04-28 01:52:48 +0200

    C - Singly linked lists

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,11 +1,12 @@
 #include "lists.h"
 
 /**
- * _strlen - returns the length of a string
- * @s: the string whose length to check
+ * _strlen - function that returns the length of a string
+ * @s: as string
  *
- * Return: integer length of string
- */
+ * Return: as it is?
+*/
+
 int _strlen(char *s)
 {
 	int l = 0;
@@ -23,6 +24,7 @@ int _strlen(char *s)
  *
  * Return: size of list
  */
+ 
 size_t print_list(const list_t *h)
 {
 	size_t i = 0;
