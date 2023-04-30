b3328ec4514c4b400efe7959c35bc94d56e0deb1
Bassem <Bassem.ibrahiem@gmail.com>
2023-04-30 08:43:35 +0300

    C - More singly linked lists

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -7,25 +7,25 @@
  */
 listint_t *find_listint_loop(listint_t *head)
 {
-	listint_t *first = head;
-	listint_t *second = head;
+	listint_t *first_loop = head;
+	listint_t *second_loop = head;
 
 	if (head == NULL)
 		return  (NULL);
 	if (head == head->next)
 		return (head);
-	while (first)
+	while (first_loop)
 	{
-		first = first->next;
-		second = head;
-		while (first && second != first)
+		first_loop = first_loop->next;
+		second_loop = head;
+		while (first_loop && second_loop != first_loop)
 		{
-			if (second == first->next)
+			if (second_loop == first_loop->next)
 			{
-				return (second);
+				return (second_loop);
 			}
-			second = second->next;
+			second_loop = second_loop->next;
 		}
 	}
-	return (first);
+	return (first_loop);
 }
